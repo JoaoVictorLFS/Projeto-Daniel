13960d3f1df74c408b9652dcb40e3beef37bbebe
João Victor <joaolfsantiago@gmail.com>
2023-06-06 16:01:58 -0300

    v1

---
+++ app.py
@@ -0,0 +1,66 @@
+from pymongo import MongoClient
+from flask import Flask, render_template, url_for, request,redirect,flash
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['usuarios']
+colection = db['users']
+
+app = Flask(__name__)
+app.secret_key = '123'
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    else:
+        user = request.form['usuario']
+        password = request.form['senha']
+
+        users = {
+            'name': user,
+        }
+
+        usuario = colection.find_one(users)
+        print(usuario)
+        if usuario:
+            if usuario['password'] == password:
+                return redirect(url_for('home'))
+            
+            else:
+                flash('Senha invalida')
+                return render_template("login.html")
+        else:
+            flash('Login ou senha invalido')
+            return redirect(url_for('login'))
+
+@app.route('/cadastro', methods=['GET','POST'])
+def add_cadastro():
+    if request.method == "GET":
+        return render_template("cadastro.html")
+    
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        
+        users = {
+            'name': name,
+            'email': email,
+            'password': password
+        }
+        
+        colection.insert_one(users)
+        
+        return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
